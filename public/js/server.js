ef660a3099eb757d0b52f5239985cf2405270dae
ChrisEvg <115230453+ChrisEvg@users.noreply.github.com>
2025-06-24 17:02:45 +0200

    added mongo online

--- public/js/server.js
+++ public/js/server.js
@@ -19,7 +19,35 @@ app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/car_rental', {
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://koder:<db_password>@cluster0.w4xwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://username:password@cluster0.mongodb.net/car_rental', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
